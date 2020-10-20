0624962c404c4fcde91dccfe4059d8ccd9e6e6dd
Brandon O <b.ondayko@gmail.com>
2020-10-20 16:12:56 -0500

    Now able to move the items between statuses dependent on what the selector is set to

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -13,13 +15,44 @@ var taskFormHandler = function(event) {
         return false;
     }
 
-    var taskDataObj = {
+    var isEdit = formEl.hasAttribute("data-task-id");
+
+    // has data attribute, so get task id and call function to complete edit process
+    if (isEdit) {
+        var taskId = formEl.getAttribute("data-task-id");
+        completeEditTask(taskNameInput, taskTypeInput, taskId);
+    } 
+    // no data attribute, so create object as normal and pass to createTaskEl function
+    else {
+        var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
+        };
+    
+        createTaskEl(taskDataObj);
+    }
+        var taskDataObj = {
+            name: taskNameInput,
+            type: taskTypeInput
     };
-    createTaskEl(taskDataObj);
+    
     formEl.reset();
 };
+
+var completeEditTask = function(taskName, taskType, taskId) {
+    //Find the mathcing task
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId +"']");
+
+    //set new values
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    alert("Task Updated!");
+
+    //reset the form
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add task";
+}
 
 var createTaskEl = function(taskDataObj) {
      // create list item
@@ -88,8 +121,6 @@ var createTaskActions = function(taskId) {
     return actionContainerEl;
 };
 
-formEl.addEventListener("submit", taskFormHandler);
-
 var taskButtonHandler = function(event) {
     var targetEl = event.target;
 
@@ -126,4 +157,29 @@ var deleteTask = function(taskId) {
     taskSelected.remove();
 };
 
+var taskStatusChangeHandler = function(event) {
+    //get the ids
+    var taskId = event.target.getAttribute("data-task-id");
+
+    //get the currently selected options and make lowercase
+    var statusValue = event.target.value.toLowerCase();
+
+    //find the parent element
+    var taskSelected = document.querySelector(".task-item[data-task-id= '"+ taskId +"']");
+
+    if (statusValue === "to-do") {
+        tasksToDoEl.appendChild(taskSelected);
+    } 
+    else if (statusValue === "in-progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+    } 
+    else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+};
+
+formEl.addEventListener("submit", taskFormHandler);
+
 pageContentEl.addEventListener("click", taskButtonHandler);
+
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
