a20606c0f84dc7eb3694719602464c6561f75d80
Brandon O <b.ondayko@gmail.com>
2020-10-20 14:05:09 -0500

    Finished the delete function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -18,7 +19,7 @@ var taskFormHandler = function(event) {
     };
     createTaskEl(taskDataObj);
     formEl.reset();
-  }
+};
 
 var createTaskEl = function(taskDataObj) {
      // create list item
@@ -42,7 +43,7 @@ listItemEl.appendChild(taskActionsEl);
 // add entire list item to list
 tasksToDoEl.appendChild(listItemEl)
 taskIdCounter++;
-}
+};
 
 var createTaskActions = function(taskId) {
     //create a Div and assign a Class
@@ -88,3 +89,19 @@ var createTaskActions = function(taskId) {
 };
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
+pageContentEl.addEventListener("click", taskButtonHandler);
